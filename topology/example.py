54856c342ba780537702102249b8a6a9494a490b
Leonardo <lbellaera@fi.uba.ar>
2021-02-15 16:29:32 -0300

    Initial commit

--- topology/example.py
+++ topology/example.py
@@ -8,23 +8,38 @@ En este caso estamos creando una topologia muy simple con la siguiente forma
 from mininet.topo import Topo
 
 class Example( Topo ):
-  def __init__( self, half_ports = 2, **opts ):
+  def __init__( self, half_ports = 2, n = 3, **opts ):
     Topo.__init__(self, **opts)
 
-    # Primero creamos los dos switches
-    sw1 = self.addSwitch('sw1')
-    sw2 = self.addSwitch('sw2')
+    switches = {}
 
-    # Luego creamos los dos hosts
-    h1 = self.addHost('h1')
-    h2 = self.addHost('h2')
+    for i in range(n):
 
-    """
-    Por ultimo, creamos los links para terminar con una topologia con la siguiente forma
-    h1 --- sw1 ---- sw2 ---- h2
-    """
-    self.addLink(sw1, sw2) # Conectando los dos switches
-    self.addLink(sw1, h1) # Conectando el sw1 con h1
-    self.addLink(sw2, h2) # Conectando el sw2 con h2
+        switches[i] = []
+        for j in range (0, 2 ** i):
+            
+            sw = self.addSwitch(f"s{2 ** i + j}")
+            switches[i].append(sw)
+
+        if i == 0:
+            continue
+
+        for sw1 in switches[i]:
+            for sw2 in switches[i-1]:
+                self.addLink(sw1, sw2)
+
+    h1 = self.addHost("h1")
+    h2 = self.addHost("h2")
+    h3 = self.addHost("h3")
+
+    self.addlink(switches[0][0], h1)
+    self.addlink(switches[0][0], h2)
+    self.addlink(switches[0][0], h3)
+
+    for i in range(2 ** (n - 1)):
+        hs = self.addHost(f"h{4 + i}")
+        self.addLink(switches[n-1][i], hs)
+
+
 
 topos = { 'example': Example }
